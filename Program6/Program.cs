4d90adb2c0da3a59a9dcc09a6653b90bdf3d0011
SB <Finn1435@rambler.ru>
2023-02-12 20:27:35 +0800

    Litle Commit:

---
+++ Program6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GeekBrains;
+
+internal class Program
+{
+    static void Main(string[] args) {
+        Console.Clear();
+
+    }
+
+    static int countPositive() {
+        String console;
+
+        Console.Clear();
+        Console.Write("Введите число: ");
+        console = Console.ReadLine();
+
+
+    }
+}
