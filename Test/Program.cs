5dfbb07d53b147822d7fd4a4a3a3c114ea718ee9
SB <Finn1435@rambler.ru>
2023-03-11 10:51:11 +0800

    Solve test programm

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GeekBrains;
+
+internal class Program
+{
+    static void Main(string[] args) {
+        Console.Clear();
+
+        int i = 0, k = 0;
+        string[] str = new string[] {"hello", "2", ":)", "world", "dot"};
+        string[] result = new string[0];
+        string[] tmp;
+
+        foreach(string s in str) {
+            if (s.Length <= 3) {
+                tmp = result;
+                result = new string[result.Length + 1];
+
+                foreach (string s1 in tmp) {
+                    result[k] = s1;
+                    k++;
+                }
+
+                k = 0;                
+                result[i] = s;
+                i++;
+            }
+        }
+
+        System.Console.WriteLine($"result array is: [{string.Join(", ", result)}]");
+    }
+}
